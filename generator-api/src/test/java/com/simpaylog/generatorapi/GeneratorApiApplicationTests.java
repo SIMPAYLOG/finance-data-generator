9858c101cb76677de9a093a3fb5839bfb4780ce9
Lee Min Woo <lmw7414@naver.com>
2025-06-20 18:12:28 +0900

    change multi module

--- generator-api/src/test/java/com/simpaylog/generatorapi/GeneratorApiApplicationTests.java
+++ generator-api/src/test/java/com/simpaylog/generatorapi/GeneratorApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
-@TestPropertySource(properties = "spring.config.name=aplication-core")
+@TestPropertySource(properties = "spring.config.name=application-core")
 @SpringBootTest
 class GeneratorApiApplicationTests {
 
