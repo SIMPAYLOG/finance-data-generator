a5a5300603883ee5099f657ac9e1a8b9cf9ba686
Lee Min Woo <lmw7414@naver.com>
2025-06-20 15:19:35 +0900

    project init

---
+++ src/main/java/com/simpaylog/finance_data_generator/FinanceDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.simpaylog.finance_data_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDataGeneratorApplication.class, args);
+	}
+
+}
