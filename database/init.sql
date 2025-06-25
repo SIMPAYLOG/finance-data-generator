1a602f853a03125039d9898cc37b670d51b2df81
Seonguk Baek <b9s2w6@naver.com>
2025-06-25 16:55:40 +0900

    #1 - fix : 테이블 컬럼 수정 및 Json 의존성 추가
    1. enum 타입 제거
    2. income_level id 속성 수정(Auto increment 불필요)

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,8 @@
-CREATE TYPE gender AS ENUM ('M', 'F');
-CREATE TYPE transaction_frequency_pattern AS ENUM ('daily', 'weekly', 'bi-weekly', 'random');
 CREATE TABLE user_behavior_profiles
 (
     id                                BIGSERIAL PRIMARY KEY,
     spending_probability              DECIMAL(2, 1),
-    transaction_frequency_pattern     transaction_frequency_pattern,
+    transaction_frequency_pattern     varchar(50),
     income_day_of_month               SMALLINT,
     auto_transfer_ratio               DECIMAL(2, 1),
     average_saving_amount_ratio       DECIMAL(2, 1),
@@ -34,7 +32,7 @@ CREATE TABLE users
     id                BIGSERIAL PRIMARY KEY,
     profile_id        BIGINT UNIQUE NOT NULL REFERENCES user_behavior_profiles (id) ON DELETE CASCADE,
     age               SMALLINT,
-    gender            gender,
+    gender            char(1),
     balance           BIGINT,
     debt              BIGINT,
     occupation_code   SMALLINT,
@@ -43,7 +41,7 @@ CREATE TABLE users
 
 CREATE TABLE income_level
 (
-    id                                BIGSERIAL PRIMARY KEY,
+    id                                BIGINT PRIMARY KEY,
     asset_range                       JSONB,
     groceries_non_alcoholic_beverages DECIMAL(5, 2),
     alcoholic_beverages_tobacco       DECIMAL(5, 2),
@@ -71,25 +69,26 @@ CREATE TABLE occupational_wages
 );
 
 -- 데이터 insert 부분
-INSERT INTO occupational_wages (occupation, monthly_wage) VALUES
-('농업, 임업 및 어업', 2878),
-('광업', 4117),
-('제조업', 3889),
-('전기, 가스, 증기 및 공기 조절 공급업', 5248),
-('수도, 하수 및 폐기물 처리, 원료 재생업', 3899),
-('건설업', 3182),
-('도매 및 소매업', 3322),
-('운수 및 창고업', 3532),
-('숙박 및 음식점업', 1830),
-('정보통신업', 4501),
-('금융 및 보험업', 5344),
-('부동산업', 2788),
-('전문, 과학 및 기술 서비스업', 4489),
-('사업시설 관리, 사업 지원 및 임대 서비스업', 3111),
-('교육 서비스업', 3106),
-('보건업 및 사회복지 서비스업', 2632),
-('예술, 스포츠 및 여가 관련 서비스업', 2575),
-('협회 및 단체, 수리 및 기타 개인 서비스업', 2547);
+INSERT INTO occupational_wages (occupation, monthly_wage)
+VALUES
+    ('농업, 임업 및 어업', 2878),
+    ('광업', 4117),
+    ('제조업', 3889),
+    ('전기, 가스, 증기 및 공기 조절 공급업', 5248),
+    ('수도, 하수 및 폐기물 처리, 원료 재생업', 3899),
+    ('건설업', 3182),
+    ('도매 및 소매업', 3322),
+    ('운수 및 창고업', 3532),
+    ('숙박 및 음식점업', 1830),
+    ('정보통신업', 4501),
+    ('금융 및 보험업', 5344),
+    ('부동산업', 2788),
+    ('전문, 과학 및 기술 서비스업', 4489),
+    ('사업시설 관리, 사업 지원 및 임대 서비스업', 3111),
+    ('교육 서비스업', 3106),
+    ('보건업 및 사회복지 서비스업', 2632),
+    ('예술, 스포츠 및 여가 관련 서비스업', 2575),
+    ('협회 및 단체, 수리 및 기타 개인 서비스업', 2547);
 
 INSERT INTO income_level (
     id,
