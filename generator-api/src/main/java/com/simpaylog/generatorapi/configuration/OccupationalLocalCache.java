07dd5becab5f8a73924edf01c55ff37642305da2
Lee Min Woo <lmw7414@naver.com>
2025-06-26 14:08:10 +0900

    #3 - feat: 직업json을 불러오기 위한 코드 작성 및 dto 설계
    서비스 실행시점에 해당 json 파일을 읽어와 캐시로 저장

---
+++ generator-api/src/main/java/com/simpaylog/generatorapi/configuration/OccupationalLocalCache.java
@@ -0,0 +1,46 @@
+package com.simpaylog.generatorapi.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simpaylog.generatorapi.dto.OccupationInfos;
+import com.simpaylog.generatorapi.dto.OccupationInfos.*;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Getter
+@Component
+public class OccupationalLocalCache {
+    private String lastUpdated;
+    private double[] ratios;
+    private final Map<Integer, Occupation> cache = new ConcurrentHashMap<>();
+
+    public Occupation get(int code) {
+        return cache.get(code);
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream input = new ClassPathResource("occupation.json").getInputStream();
+            OccupationInfos data = mapper.readValue(input, OccupationInfos.class);
+            lastUpdated = data.lastUpdated();
+            ratios = data.ratios();
+            Map<Integer, Occupation> map = new HashMap<>();
+
+            for(Occupation occ : data.occupations()) {
+                map.put(occ.code(), occ);
+            }
+            cache.putAll(map);
+            System.out.println("[OccupationCacheInitializer] 캐시 로딩 완료: " + map.size() + "개");
+        } catch (Exception e) {
+            System.err.println("캐시 초기화 중 오류 발생: " + e.getMessage());
+        }
+    }
+}
