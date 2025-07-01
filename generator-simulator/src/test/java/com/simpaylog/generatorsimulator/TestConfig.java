2c1da9b732d71bc496a75df0c562a61e9246230e
KHJHK <tom1997tom@gmail.com>
2025-07-01 09:24:25 +0900

    #5 - refactor : test code 수정 및 simulator의 gradle 수정
    test code 수정 및 simulator의 gradle 수정

--- generator-simulator/src/test/java/com/simpaylog/generatorsimulator/TestConfig.java
+++ generator-simulator/src/test/java/com/simpaylog/generatorsimulator/TestConfig.java
@@ -1,6 +1,5 @@
 package com.simpaylog.generatorsimulator;
 
-import com.simpaylog.generatorapi.configuration.IncomeLevelLocalCache;
 import com.simpaylog.generatorcore.GeneratorCoreConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,6 +7,6 @@ import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @TestPropertySource(properties = "spring.config.name=application-core")
-@SpringBootTest(classes = {GeneratorCoreConfiguration.class, IncomeLevelLocalCache.class})
+@SpringBootTest(classes = GeneratorCoreConfiguration.class)
 public class TestConfig {
 }
--- generator-simulator/src/test/java/com/simpaylog/generatorsimulator/configuration/PreferenceInfosLocalCacheTest.java
+++ generator-simulator/src/test/java/com/simpaylog/generatorsimulator/configuration/PreferenceInfosLocalCacheTest.java
@@ -1,31 +1,29 @@
 package com.simpaylog.generatorsimulator.configuration;
 
-import com.simpaylog.generatorsimulator.dto.PreferenceInfos;
+import com.simpaylog.generatorsimulator.TestConfig;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = {PreferenceLocalCache.class})
-public class PreferenceInfosLocalCacheTest {
+@Import(PreferenceLocalCache.class)
+public class PreferenceInfosLocalCacheTest extends TestConfig {
      private final String[] preferenceNames = new String[] {"", "소비지향형", "저축지향형", "무계획형", "투자지향형", "안정추구형"};
 
      @Autowired
      PreferenceLocalCache preferenceCache;
 
-     @Test
+     @RepeatedTest(value = 5)
      @DisplayName("preference.json → 객체 매핑 검증")
-     void loadPreferencesTest() {
-         Map<Integer, PreferenceInfos> preferences = preferenceCache.getAll();
-         assertNotNull(preferences);
-         for(int i = 1; i <= 5; i++){
-             assertEquals(preferenceNames[i], preferences.get(i).name());
-             assertNotNull(preferences.get(i).totalConsumeRange());
-             assertFalse(preferences.get(i).tagConsumeRange().isEmpty());
-             assertEquals(12, preferences.get(i).tagConsumeRange().size());
-         }
+     void loadPreferencesTest(RepetitionInfo repetitionInfo) {
+         assertNotNull(preferenceCache.getAll());
+         int preferenceId = repetitionInfo.getCurrentRepetition();
+         assertEquals(preferenceNames[preferenceId], preferenceCache.get(preferenceId).name());
+         assertNotNull(preferenceCache.get(preferenceId).totalConsumeRange());
+         assertFalse(preferenceCache.get(preferenceId).tagConsumeRange().isEmpty());
+         assertEquals(12, preferenceCache.get(preferenceId).tagConsumeRange().size());
      }
 }
