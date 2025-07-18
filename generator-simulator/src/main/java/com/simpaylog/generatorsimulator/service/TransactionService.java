279345395e705122896ad55246eac42813c465c1
Lee Min Woo <lmw7414@naver.com>
2025-07-19 01:56:23 +0900

    #35 - feat: 급여 로직을 포함한 트랜잭션 생성 메서드 보완
    급여 정보를 담은 캐시로 부터 날짜를 가져와 해당 날짜일 경우 급여를 제공하는 코드 추가함.

--- generator-simulator/src/main/java/com/simpaylog/generatorsimulator/service/TransactionService.java
+++ generator-simulator/src/main/java/com/simpaylog/generatorsimulator/service/TransactionService.java
@@ -1,21 +1,27 @@
 package com.simpaylog.generatorsimulator.service;
 
-import com.simpaylog.generatorcore.entity.User;
+import com.simpaylog.generatorcore.entity.dto.TransactionUserDto;
+import com.simpaylog.generatorcore.enums.WageType;
+import com.simpaylog.generatorcore.repository.PaydayCache;
 import com.simpaylog.generatorcore.service.UserService;
 import com.simpaylog.generatorsimulator.dto.*;
+import com.simpaylog.generatorsimulator.exception.SimulatorException;
 import com.simpaylog.generatorsimulator.producer.TransactionLogProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
@@ -23,26 +29,38 @@ public class TransactionService {
     private final TradeGenerator tradeGenerator;
     private final TransactionGenerator transactionGenerator;
     private final TransactionLogProducer transactionLogProducer;
+    private final PaydayCache paydayCache;
 
-    public DailyTransactionResult generateTransaction(User user, LocalDate date) {
+    public DailyTransactionResult generateTransaction(TransactionUserDto dto, LocalDate date) {
         Map<CategoryType, Long> spendingByCategory = new HashMap<>();
         long totalSpending = 0L;
-        int totalCnt = 0;
-
+        int totalSpendingCnt = 0;
+        long totalIncome = 0L;
+        int totalIncomeCnt = 0;
+        boolean salaryFlag = false;
         try {
             LocalDateTime from = date.atStartOfDay();
             LocalDateTime to = date.atTime(23, 59);
             Map<CategoryType, LocalDateTime> lastUsedMap = new HashMap<>();
-            PreferenceType userPreference = PreferenceType.fromKey(user.getUserBehaviorProfile().getPreferenceId());
-            int userDecile = user.getDecile();
-            BigDecimal userBalance = user.getBalance();
+            PreferenceType userPreference = PreferenceType.fromKey(dto.preferenceId());
+            int userDecile = dto.decile();
+            BigDecimal userBalance = dto.balance();
 
             long hours = ChronoUnit.HOURS.between(from, to);
             for (int hour = 0; hour <= hours; hour++) {
-                // TODO: 공과금, 통신비, 월급 처리
+                // TODO: 공과금, 통신비
                 int[] minutes = getRandomMinutes();
                 for (int mIdx = 0; mIdx < minutes.length; mIdx++) {
                     LocalDateTime curTime = from.plusHours(hour).plusMinutes(minutes[mIdx]);
+
+                    if (!salaryFlag && curTime.getHour() >= 8) { // 급여 지급 결정, 8시 이후로 지급
+                        BigDecimal newBalance = handleSalary(dto, curTime);
+                        totalIncome += newBalance.longValue();
+                        totalIncomeCnt++;
+                        salaryFlag = !newBalance.equals(userBalance);
+                        userBalance = newBalance;
+                    }
+
                     CategoryType picked = transactionGenerator.pickOneCategory(curTime, userPreference, lastUsedMap).orElse(null);
                     if (picked == null) { // 해당 시간에 데이터가 발생하지 않음
                         continue;
@@ -51,27 +69,61 @@ public class TransactionService {
                     lastUsedMap.put(picked, curTime);
                     Trade userTrade = tradeGenerator.generateTrade(userDecile, picked);
                     updateMap(spendingByCategory, picked, userTrade.cost()); // 리턴용: 현재 상황 업데이트
-                    TransactionLog withdrawLog = TransactionLog.withdraw(
-                            user.getId(),
-                            curTime,
-                            userTrade.tradeName(),
-                            userTrade.cost(),
-                            userBalance,
-                            userBalance.subtract(userTrade.cost())
-                    );
+
                     userBalance = userBalance.subtract(userTrade.cost());
                     totalSpending += userTrade.cost().longValue();
-                    totalCnt++;
-
-                    transactionLogProducer.send(withdrawLog);
-
+                    totalSpendingCnt++;
+                    generateMessage(dto.userId(), curTime, TransactionLog.TransactionType.WITHDRAW, userTrade.tradeName(), userTrade.cost(), userBalance, userBalance.subtract(userTrade.cost()));
                 }
             }
-            userService.updateUserBalance(user.getId(), userBalance);
-            return DailyTransactionResult.success(user.getId(), date, totalCnt, totalSpending, spendingByCategory);
+            userService.updateUserBalance(dto.userId(), userBalance);
+            return DailyTransactionResult.success(dto.userId(), date, totalSpendingCnt, totalSpending, spendingByCategory, totalIncomeCnt, totalIncome);
         } catch (Exception e) {
-            return DailyTransactionResult.fail(user.getId(), date, totalCnt, totalSpending, spendingByCategory, e.getMessage());
+            return DailyTransactionResult.fail(dto.userId(), date, totalSpendingCnt, totalSpending, spendingByCategory, totalIncomeCnt, totalIncome, e.getMessage());
         }
+    }
+
+
+    private BigDecimal handleSalary(TransactionUserDto user, LocalDateTime current) {
+        int numberOfPaydays = paydayCache.numberOfPaydays(user.userId(), YearMonth.from(current));
+        if (numberOfPaydays == 0 || !paydayCache.isPayday(user.userId(), YearMonth.from(current), LocalDate.from(current)))
+            return user.balance();
+
+        double averageSalary = user.incomeValue().doubleValue();
+        WageType userWageType = user.wageType();
+
+        BigDecimal wage = BigDecimal.valueOf(gaussianRandom(averageSalary / numberOfPaydays, averageSalary * userWageType.getVolatility() / numberOfPaydays));
+        BigDecimal updatedBalance = user.balance().add(wage);
+        generateMessage(user.userId(), current, TransactionLog.TransactionType.DEPOSIT, "급여 입금", wage, user.balance(), updatedBalance);
+        return updatedBalance;
+    }
+
+    private void generateMessage(Long userId, LocalDateTime timestamp, TransactionLog.TransactionType type, String description, BigDecimal amount, BigDecimal balanceBefore, BigDecimal balanceAfter) {
+        TransactionLog transactionLog = TransactionLog.of(
+                userId,
+                timestamp,
+                type,
+                description,
+                amount,
+                balanceBefore,
+                balanceAfter
+        );
+        try {
+            transactionLogProducer.send(transactionLog);
+        } catch (Exception e) {
+            // TODO: 필요 시 fallback 로직: DB 적재, 재시도 큐, 알림 등
+            log.error("[Kafka Send Fail] userId={}, type={}, msg={}", userId, type, e.getMessage());
+            throw new SimulatorException("카프카 데이터 전송 실패");
+        }
+    }
+
+    /**
+     * @param mean   평균 값
+     * @param stdDev 조정 수치(작을수록: 대부분의 값이 평균에 가까움 (안정적, 예측 가능) | 클수록: 값들이 평균에서 멀리 퍼짐 (변동성 높음, 불규칙))
+     * @return 급여 금액
+     */
+    private double gaussianRandom(double mean, double stdDev) {
+        return mean + ThreadLocalRandom.current().nextGaussian() * stdDev;
     }
 
     // 최대 3개의 분 뽑기
@@ -84,7 +136,8 @@ public class TransactionService {
     }
 
     private void updateMap(Map<CategoryType, Long> spendingByCategory, CategoryType categoryType, BigDecimal spending) {
-        spendingByCategory.put(categoryType, spendingByCategory.getOrDefault(categoryType, 0L) + spending.longValue());
+        long cost = spending.longValue();
+        spendingByCategory.merge(categoryType, cost, Long::sum);
     }
 
 }
