d650d96d1448238042f78262e5fb20170af30102
Lee Min Woo <lmw7414@naver.com>
2025-07-19 01:50:47 +0900

    #35 - feat: 유저 급여일 저장을 위한 Cache 생성
    급여의 경우 월급, 일급, 주급, 랜덤의 형태로 형태에 따라 0번 이상의 급여가 발생할 수 있으나 최대 급여 횟수는 달에 정해져있다. 따라서 사용자의 월급일을 서비스 시작 전에 기록하고 날짜에 맞춰 지급하기 위해 캐시를 생성한다.
    
    급여 금액의 경우 한달을 평균으로 잡았을 때의 금액이기에 항상 해당 금액에 일치하기는 어렵다고 판단했고, 가우시안 랜덤 로직을 적용해 매번 다르게 받지만 그 근처의 금액에 할당될 수 있도록 설정했다.

--- generator-core/src/main/java/com/simpaylog/generatorcore/strategy/WageStrategyExecutor.java
+++ generator-core/src/main/java/com/simpaylog/generatorcore/strategy/WageStrategyExecutor.java
@@ -1,10 +1,12 @@
 package com.simpaylog.generatorcore.strategy;
 
 import com.simpaylog.generatorcore.entity.User;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Component
 public class WageStrategyExecutor {
 
     public List<LocalDate> decidePayout(User user, LocalDate baseDate) {
