b0aa2f5bef9c3af71be6cbfd2cd5ec81334444f0
Lee Min Woo <lmw7414@naver.com>
2025-07-19 01:55:02 +0900

    #35 - feat: generateTransaction 메서드의 파라미터 변경 및 리턴 타입 수익 정보 추가
    DB와 연결되는 User 엔티티를 직접 사용하는 것보다는 별도의 DTO를 두어 사용하기로 함.
    
    리턴 타입의 경우 급여 정보가 포함됨에 따라 필드를 추가하였음

--- generator-simulator/src/main/java/com/simpaylog/generatorsimulator/dto/DailyTransactionResult.java
+++ generator-simulator/src/main/java/com/simpaylog/generatorsimulator/dto/DailyTransactionResult.java
@@ -6,18 +6,20 @@ import java.util.Map;
 public record DailyTransactionResult(
         Long userId,
         LocalDate date,
-        Integer transactionCount,
+        Integer spendingTransactionCount,
         Long totalSpending,
         Map<CategoryType, Long> spendingByCategory,
+        Integer incomeTransactionCount,
+        Long totalIncome,
         boolean success,
         String errorMessage
 ) {
 
-    public static DailyTransactionResult success(Long userId, LocalDate date, int transactionCount, long totalSpending, Map<CategoryType, Long> spendingByCategory) {
-        return new DailyTransactionResult(userId, date, transactionCount, totalSpending, spendingByCategory, true, null);
+    public static DailyTransactionResult success(Long userId, LocalDate date, int spendingTransactionCount, long totalSpending, Map<CategoryType, Long> spendingByCategory, int incomeTransactionCount, long totalIncome) {
+        return new DailyTransactionResult(userId, date, spendingTransactionCount, totalSpending, spendingByCategory, incomeTransactionCount, totalIncome, true, null);
     }
 
-    public static DailyTransactionResult fail(Long userId, LocalDate date, int transactionCount, long totalSpending, Map<CategoryType, Long> spendingByCategory, String errorMessage) {
-        return new DailyTransactionResult(userId, date, transactionCount, totalSpending, spendingByCategory, false, errorMessage);
+    public static DailyTransactionResult fail(Long userId, LocalDate date, int spendingTransactionCount, long totalSpending, Map<CategoryType, Long> spendingByCategory, int incomeTransactionCount, long totalIncome, String errorMessage) {
+        return new DailyTransactionResult(userId, date, spendingTransactionCount, totalSpending, spendingByCategory, incomeTransactionCount, totalIncome, false, errorMessage);
     }
 }
